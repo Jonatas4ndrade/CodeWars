a0d3d8feac339491c341c2336118c05fb03006e2
Jônatas S. Andrade <jonatassilveiraandrade@gmail.com>
2021-12-12 18:43:00 -0300

    Improved readability.

--- 1 - String Reversal/String to CamelCase (Python).py
+++ 1 - String Reversal/String to CamelCase (Python).py
@@ -1,15 +1,20 @@
 def to_camel_case(text):
-    # Returns empty string.
+    
+    # Returns empty string, if empty.
     if not text:
         return text
+    
     # Return value is built here.
     camelCase = ""
+    
     # If true, next char will be converted to uppercase.
     slashFlag = False
+    
     for char in text:
         if slashFlag:
             camelCase += char.upper()
             slashFlag = False
+        # If slash or underscore, set flag and continue    
         elif char == "-" or char == "_":
             slashFlag = True
         else:
